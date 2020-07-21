79914bf7327ebf50368a776753d6ad936997fb7a
AndreasHGK <36563463+AndreasHGK@users.noreply.github.com>
2020-07-21 21:18:26 +0200

    fix array

--- src/main/java/com/sukkit/Sukkit.java
+++ src/main/java/com/sukkit/Sukkit.java
@@ -3,13 +3,14 @@ package com.sukkit;
 public class Sukkit {
 
     public static void main(String[] args) {
-        String[] files = [
+        String[] files = {
                 "https://github.com/pmmp/PocketMine-MP/releases/download/3.14.2/PocketMine-MP.phar",
                 "https://github.com/pmmp/PocketMine-MP/releases/download/3.14.2/start.cmd",
-            ];
-        for (String (file) : files) {
+        };
+        for (String file : files) {
             URL url = new URL(file);
-            BufferedInputStream in = new BufferedInputStream(url.openStream());
+            ReadableByteChannel in = Channels.newChannel(url.openStream());
+            FileOuputStream out = new FileOutputStream("")
         }
     }
 
